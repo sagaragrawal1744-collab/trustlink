35869ab791103a7f0b87e56900cd8726a8e0ee78
Sagar Agrawal <sagaragrawal1744@gmail.com>
2026-03-29 21:43:02 +0530

    fix login+dataloader+valiadation

--- trustlink/src/main/java/com/trustlink/DataLoader.java
+++ trustlink/src/main/java/com/trustlink/DataLoader.java
@@ -13,27 +13,34 @@ public class DataLoader {
     @Bean
     CommandLineRunner initUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
+
             if (userRepository.findByEmail("user@gmail.com").isEmpty()) {
                 User user = new User();
+                user.setName("Demo User");
                 user.setEmail("user@gmail.com");
                 user.setPassword(passwordEncoder.encode("1234"));
                 user.setRole("USER");
+                user.setStatus("ACTIVE");
                 userRepository.save(user);
             }
 
             if (userRepository.findByEmail("provider@gmail.com").isEmpty()) {
                 User provider = new User();
+                provider.setName("Demo Provider");
                 provider.setEmail("provider@gmail.com");
                 provider.setPassword(passwordEncoder.encode("1234"));
                 provider.setRole("PROVIDER");
-            userRepository.save(provider);
+                provider.setStatus("ACTIVE");
+                userRepository.save(provider);
             }
 
             if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
                 User admin = new User();
+                admin.setName("Demo Admin");
                 admin.setEmail("admin@gmail.com");
                 admin.setPassword(passwordEncoder.encode("admin1234"));
                 admin.setRole("ADMIN");
+                admin.setStatus("ACTIVE");
                 userRepository.save(admin);
             }
         };
